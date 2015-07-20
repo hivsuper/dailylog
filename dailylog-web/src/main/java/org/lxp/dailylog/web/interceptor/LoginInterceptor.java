2c014dbbb45f214fcecdd60872148884927c790a
Super Li <xupenglee@gmail.com>
2015-07-21 01:19:41 +0800

    2015-7-20 : modify LoginInterceptor configuration

--- dailylog-web/src/main/java/org/lxp/dailylog/web/interceptor/LoginInterceptor.java
+++ dailylog-web/src/main/java/org/lxp/dailylog/web/interceptor/LoginInterceptor.java
@@ -3,7 +3,6 @@ package org.lxp.dailylog.web.interceptor;
 import static org.lxp.dailylog.web.util.StringHolder.USER;
 
 import java.io.PrintWriter;
-import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,8 +16,6 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  * @version 17 Feb 2012
  */
 public class LoginInterceptor extends HandlerInterceptorAdapter {
-  private Set<String> excludes;
-  private Set<String> excludePaths;
   private String ajaxPath;
   private static final String SLASH = "/";
 
@@ -26,63 +23,29 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
     boolean result = true;
     String uri = request.getServletPath();
-    if (!SLASH.equals(uri)) {
-      HttpSession session = request.getSession();
-      if (uri.endsWith(SLASH)) {
-        response.sendRedirect(request.getContextPath() + uri.substring(0, uri.length() - 1));
-      } else if (this.exclude(uri)) {
-        result = true;
+    HttpSession session = request.getSession();
+    if (uri.endsWith(SLASH)) {
+      response.sendRedirect(request.getContextPath() + uri.substring(0, uri.length() - 1));
+    } else {
+      boolean isLogin = session.getAttribute(USER) != null;
+      if (uri.endsWith(ajaxPath) && !isLogin) {
+        PrintWriter out = response.getWriter();
+        out.write("403");
+        out.flush();
       } else {
-        boolean isLogin = session.getAttribute(USER) != null;
-        if (uri.endsWith(ajaxPath) && !isLogin) {
-          PrintWriter out = response.getWriter();
-          out.write("403");
-          out.flush();
+        if (!isLogin) {
+          response.sendRedirect(request.getContextPath() + "/404");
         } else {
-          if (!isLogin) {
-            response.sendRedirect(request.getContextPath() + "/404");
-          } else {
-            result = isLogin;
-          }
+          result = isLogin;
         }
       }
     }
     return result;
   }
 
-  private boolean exclude(String uri) {
-    for (String path : excludes) {
-      if (uri.endsWith(path)) {
-        return true;
-      }
-    }
-    for (String path : excludePaths) {
-      if (uri.contains(path)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
   @Override
   public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView mav)
       throws Exception {
-  }
-
-  public Set<String> getExcludes() {
-    return excludes;
-  }
-
-  public void setExcludes(Set<String> excludes) {
-    this.excludes = excludes;
-  }
-
-  public Set<String> getExcludePaths() {
-    return excludePaths;
-  }
-
-  public void setExcludePaths(Set<String> excludePaths) {
-    this.excludePaths = excludePaths;
   }
 
   public String getAjaxPath() {
