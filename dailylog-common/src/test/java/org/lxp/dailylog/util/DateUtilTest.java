c13ca26c2e00004067bdc369e9079a3e4906483b
Super.Li <xupenglee@gmail.com>
2017-08-22 22:39:37 +0800

    1. upgrade mysql-connector-java to 5.1.42 2. code clean

--- dailylog-common/src/test/java/org/lxp/dailylog/util/DateUtilTest.java
+++ dailylog-common/src/test/java/org/lxp/dailylog/util/DateUtilTest.java
@@ -2,15 +2,11 @@ package org.lxp.dailylog.util;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.lxp.dailylog.util.DateUtil;
-import org.mockito.runners.MockitoJUnitRunner;
 
 /**
  * @author Super.Li
  * @since 2015年7月19日
  */
-@RunWith(MockitoJUnitRunner.class)
 public class DateUtilTest {
 
   @Test
