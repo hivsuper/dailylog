08b8739e987de1f838aae3f92889e71c7b64757d
Super Li <xupenglee@gmail.com>
2018-05-11 00:53:10 -0500

    code clean

--- dailylog-web/src/main/java/org/lxp/dailylog/config/DataSourceConfig.java
+++ dailylog-web/src/main/java/org/lxp/dailylog/config/DataSourceConfig.java
@@ -10,7 +10,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -18,7 +17,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan(basePackages = DataSourceConfig.BASE_PACKAGES, sqlSessionTemplateRef = DataSourceConfig.SQL_SESSION_TEMPLATE)
 public class DataSourceConfig {
     public static final String BASE_PACKAGES = "org.lxp.dailylog.dao.mapper";
-    public static final String MAPPER_XML_PATH = "classpath:org/lxp/dailylog/dao/mapper/*/*.xml";
     public static final String SQL_SESSION_TEMPLATE = "sqlSessionTemplate";
     private static final String SQL_SESSION_FACTORY = "sqlSessionFactory";
     @Resource
@@ -28,7 +26,6 @@ public class DataSourceConfig {
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
         bean.setDataSource(dataSource);
-        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(MAPPER_XML_PATH));
         return bean.getObject();
     }
 
