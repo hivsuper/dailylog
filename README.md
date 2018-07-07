69265ddb71b3276c2c23544938983c8172c42100
Super Li <xupenglee@gmail.com>
2018-07-07 17:52:10 +0800

    code clean

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # dailylog-backend
 *How to run the project?*
 1) Run command `mvn clean install` on dailylog-parent  
-2) Run `org.lxp.dailylog.Bootstrap` in dailylog-web  
+2) Run `org.lxp.dailylog.Bootstrap` in dailylog-web or comman `mvn spring-boot:run` on dailylog-web  
 
 *How to generate mapper.xml and mapper classes?*
 + Run command `mvn install` on dailylog-common  
--- dailylog-web/src/main/java/org/lxp/dailylog/config/DataSourceConfig.java
+++ dailylog-web/src/main/java/org/lxp/dailylog/config/DataSourceConfig.java
@@ -1,10 +1,5 @@
 package org.lxp.dailylog.config;
 
-import java.util.Properties;
-
-import javax.annotation.Resource;
-import javax.sql.DataSource;
-
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -19,15 +14,19 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.interceptor.TransactionInterceptor;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.util.Properties;
+
 @Configuration
 @EnableTransactionManagement
-@ComponentScan(basePackages = { DataSourceConfig.BASE_PACKAGES })
+@ComponentScan(basePackages = {DataSourceConfig.BASE_PACKAGES})
 @MapperScan(basePackages = DataSourceConfig.MAPPER_PACKAGES, sqlSessionTemplateRef = DataSourceConfig.SQL_SESSION_TEMPLATE)
 public class DataSourceConfig {
-    public static final String BASE_PACKAGES = "org.lxp.dailylog";
-    public static final String MAPPER_PACKAGES = "org.lxp.dailylog.dao.mapper";
-    public static final String SQL_SESSION_TEMPLATE = "sqlSessionTemplate";
-    public static final String TRANSACTION_MANAGER = "transactionManager";
+    static final String BASE_PACKAGES = "org.lxp.dailylog";
+    static final String MAPPER_PACKAGES = "org.lxp.dailylog.dao.mapper";
+    static final String SQL_SESSION_TEMPLATE = "sqlSessionTemplate";
+    private static final String TRANSACTION_MANAGER = "transactionManager";
     private static final String SQL_SESSION_FACTORY = "sqlSessionFactory";
     private static final String TX_ADVICE = "txAdvice";
     @Resource
@@ -46,14 +45,12 @@ public class DataSourceConfig {
     }
 
     @Bean(name = SQL_SESSION_TEMPLATE)
-    public SqlSessionTemplate sqlSessionTemplate(@Qualifier(SQL_SESSION_FACTORY) SqlSessionFactory sqlSessionFactory)
-            throws Exception {
+    public SqlSessionTemplate sqlSessionTemplate(@Qualifier(SQL_SESSION_FACTORY) SqlSessionFactory sqlSessionFactory) {
         return new SqlSessionTemplate(sqlSessionFactory);
     }
 
     @Bean
-    public AspectJExpressionPointcutAdvisor pointcutAdvisor(
-            @Qualifier(TX_ADVICE) TransactionInterceptor transactionInterceptor) {
+    public AspectJExpressionPointcutAdvisor pointcutAdvisor(@Qualifier(TX_ADVICE) TransactionInterceptor transactionInterceptor) {
         AspectJExpressionPointcutAdvisor pointcutAdvisor = new AspectJExpressionPointcutAdvisor();
         pointcutAdvisor.setAdvice(transactionInterceptor);
         pointcutAdvisor.setExpression("execution (* org.lxp.dailylog..service.*.*(..))");
@@ -64,8 +61,7 @@ public class DataSourceConfig {
      * http://www.roncoo.com/article/detail/129836
      */
     @Bean(name = TX_ADVICE)
-    public TransactionInterceptor transactionInterceptor(
-            @Qualifier(TRANSACTION_MANAGER) PlatformTransactionManager transactionManager) throws Exception {
+    public TransactionInterceptor transactionInterceptor(@Qualifier(TRANSACTION_MANAGER) PlatformTransactionManager transactionManager) {
         Properties attributes = new Properties();
         attributes.setProperty("add*", "PROPAGATION_REQUIRED,ISOLATION_DEFAULT");
         attributes.setProperty("update*", "PROPAGATION_REQUIRED,ISOLATION_DEFAULT");
