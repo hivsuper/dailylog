fbf15b7ddef37cbdd3ee862aabae5d9bfd0714f0
Super Li <xupenglee@gmail.com>
2018-07-08 13:02:39 +0800

    complete login.html

--- dailylog-admin/js/common.js
+++ dailylog-admin/js/common.js
@@ -1,3 +1,85 @@
 var v={
-    apiHost : 'http://127.0.0.1:8080/dailylog/'
+    apiHost : 'http://127.0.0.1:8080/dailylog/',
+    go : function(url) {
+        if(url.indexOf("/")==0) {
+            layer.alert('Do not use url which start with /');
+        }else{
+            top.location.href = this._getRootPath_dc() + '/' + url;
+        }
+    },
+    _getRootPath_dc : function() {
+        var pathName = top.location.pathname.substring(1);
+        var webName = pathName == '' ? '' : pathName.substring(0, pathName.indexOf('/'));
+        if (webName == "") {
+            return top.location.protocol + '//' + top.location.host;
+        } else {
+            return top.location.protocol + '//' + top.location.host + '/' + webName;
+        }
+    },
+    isEmpty : function(str) {
+        return $.trim(str).length==0;
+    },
+    heartBeat : function() {
+        this.ajaxGetJsonResult(this.apiHost + 'heartBeat.json', undefined, undefined, undefined, false);
+    },
+    ajaxGetJsonResult: function(url, data, success, error, shade) {
+        var option = this.createOption(url, 'get', data, success, error, true, shade);
+        $.ajax(option);
+    },
+    ajaxPostJsonResult: function(url, data, success, error, shade) {
+        var option = this.createOption(url, 'post', data, success, error, true, shade);
+        $.ajax(option);
+    },
+    createOption: function(url, type, data, success, error, async, shade) {
+        if (shade == undefined || shade == null || shade) {
+            layer.load(1, {
+                shade: [0.6, '#000'] //0.6透明度的白色背景
+            });
+        }
+        var that = this;
+        var option = {
+            url: url,
+            data: data || {},
+            type: type || 'post',
+            dataType: 'json',
+            cache: false,
+            async: async || false,
+            xhrFields: {
+                withCredentials: true
+            },
+            complete: function() {
+                layer.closeAll('loading');
+            },
+            success: function(json) {
+                var code = json.code;
+                if (200 == code) {
+                    var data = json.content;
+                    success(data);
+                } else if (403 == code) {
+                    that.go('login.html');
+                } else {
+                    if (error) {
+                        error(code);
+                    } else {
+                        alert('Error Code=' + code);
+                    }
+                }
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+            }
+        };
+        return option;
+    },
+    _writeJson: function(key, json) {
+        localStorage.setItem(key, JSON.stringify(json));
+    },
+    _getJson: function(key) {
+        return localStorage.getItem(key);
+    },
+    getUser: function() {
+        return eval("(" + this._getJson('dailylog_admin_user') + ")");
+    },
+    setUser: function(user) {
+        return this._writeJson('dailylog_admin_user', user);
+    }
 }
